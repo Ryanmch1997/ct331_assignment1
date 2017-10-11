191bec237fb00a29aaf59e80085e5533013074fb
Ryanmch1997 <ryanmch1997@eircom.net>
2017-10-11 19:23:04 +0100

    Add files via upload

---
+++ src/q1/assignment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int arg, char* argc[]){
+  printf("%d\n", sizeof(char**));
+  printf("%d\n", sizeof(int));
+  printf("%d\n", sizeof(int*));
+  printf("%d\n", sizeof(double*));
+  printf("%d\n", sizeof(long));
+  return 0;
+}
